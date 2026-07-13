437aae62618253f68f5fda8142f75fe0c1c431b2
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-14 00:27:49 +0200

    add entity annotation on Role entity

--- src/main/java/com/example/demo/Model/Role.java
+++ src/main/java/com/example/demo/Model/Role.java
@@ -1,9 +1,6 @@
 package com.example.demo.Model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "roles")
 public class Role {
 
