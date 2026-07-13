c179ea745bc4d36816489ebfc9ebe16aeee6e611
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-14 00:22:57 +0200

    add ManyToMany relationship to users and role

--- src/main/java/com/example/demo/Model/User.java
+++ src/main/java/com/example/demo/Model/User.java
@@ -3,7 +3,8 @@ package com.example.demo.Model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.loader.NonUniqueDiscoveredSqlAliasException;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -21,5 +22,13 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+
+    private Set<Role> roles;
 
 }
