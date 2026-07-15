45ba75551d0a2f1558c5745704baa4e3374d117b
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-15 15:54:43 +0200

    add AuthController

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.JWTAuthResponse;
+import com.example.demo.dto.LoginDto;
+import com.example.demo.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JWTAuthResponse> authenticate(@RequestBody LoginDto loginDto){
+
+        String token = authService.login(loginDto);
+
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+}
